ae94702384fd53c0e29560de00e6d6a82e43eac6
maikisinovsky <maia.kisinovsky@gmail.com>
2025-01-05 17:08:31 -0300

    feat: registro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar.jsx';
+import Signup from './pages/signup.js';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <button></button>
+      <button></button>
+      <button></button>
+    </div>
+  )
+}
+
+export default Navbar
