ec604db59dd3429e2da0ac6b74837f378319b95e
maikisinovsky <maia.kisinovsky@gmail.com>
2025-01-07 16:33:47 -0300

    chore: css landing page

--- frontend/src/pages/Start.jsx
+++ frontend/src/pages/Start.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import '../styles/Start.css'
+import { Link } from 'react-router-dom'
 
 function Start() {
   return (
     <div className='background-one'>
         <main className='logo-start'>
-           <div className='space'></div> 
-           <button>Crea un pedido <div className='order-img'></div></button>
+
+            <div className='space'></div> 
+
+            <section className='section-button-start'>
+                <Link to={"/signup"} className='link-no-decoration'>
+                    <button className='button-start'>Crea un pedido 
+                        <div className='order-img'></div>
+                    </button>
+                </Link>
+                    
+            </section>
+
         </main>
     </div>
   )
