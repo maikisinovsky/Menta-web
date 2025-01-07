3ba5b3e98d888b0e639dbc4b66f120cc5d7b6519
maikisinovsky <maia.kisinovsky@gmail.com>
2025-01-07 15:56:59 -0300

    chore: estructura de la landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login.jsx';
 import Navbar from './components/Navbar.jsx';
 import Signup from './pages/signup.jsx';
+import Start from './pages/Start.jsx';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+          <Route path="/" exact element={<Start/>} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/Login" element={<Login />} />
         </Routes>
---
+++ frontend/src/pages/Start.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Start.css'
+
+function Start() {
+  return (
+    <div className='background-one'>
+        <main className='logo-start'>
+           <div className='space'></div> 
+           <button>Crea un pedido <div className='order-img'></div></button>
+        </main>
+    </div>
+  )
+}
+
+export default Start
